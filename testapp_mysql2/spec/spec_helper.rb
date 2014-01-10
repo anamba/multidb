fb3fabe709690182ea76f55d1930096ded28e115
Aaron Namba <aaron@biggerbird.com>
2014-01-10 13:22:58 +0900

    fixed simplecov by requiring it before models

--- testapp_mysql2/spec/spec_helper.rb
+++ testapp_mysql2/spec/spec_helper.rb
@@ -1,9 +1,9 @@
 ENV["RAILS_ENV"] ||= 'test'
+require 'simplecov'
 require File.expand_path("../../config/environment", __FILE__)
 
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'simplecov'
 # require 'factory_girl_rails'
 
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
