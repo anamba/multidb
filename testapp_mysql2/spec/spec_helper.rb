5753272df30df5bcb94d91795f9d783e052fc158
Aaron Namba <aaron@biggerbird.com>
2014-01-10 13:11:47 +0900

    removed unused gems

--- testapp_mysql2/spec/spec_helper.rb
+++ testapp_mysql2/spec/spec_helper.rb
@@ -4,9 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'simplecov'
-require 'factory_girl_rails'
+# require 'factory_girl_rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.order = "random"
